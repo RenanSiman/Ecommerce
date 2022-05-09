ba68917911b7a80cb7c1b0f14f47199bd6d959a9
RenanSiman <renanvinte2@hotmail.com>
2022-05-09 00:49:22 -0300

    Create components Product and Rating

--- mycommerce/frontend/src/App.js
+++ mycommerce/frontend/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import data from "./data";
+import Product from "./components/Product";
 
 function App() {
   return (
@@ -18,34 +19,7 @@ function App() {
       <main>
         <div className="row center">
           {data.products.map((prod) => (
-            <div key={prod._id} className="card">
-              <a href={`/product/${prod._id}`}>
-                <img className="medium" src={prod.image} alt={prod.codename} />
-              </a>
-              <div className="card-body">
-                <a href="product.html">
-                  <h2>{prod.codename}</h2>
-                </a>
-                <div className="rating">
-                  <span>
-                    <i className="fa fa-star"></i>
-                  </span>
-                  <span>
-                    <i className="fa fa-star"></i>
-                  </span>
-                  <span>
-                    <i className="fa fa-star"></i>
-                  </span>
-                  <span>
-                    <i className="fa fa-star"></i>
-                  </span>
-                  <span>
-                    <i className="fa fa-star-half"></i>
-                  </span>
-                </div>
-                <div className="price">R$ {prod.price.toFixed(2)}</div>
-              </div>
-            </div>
+            <Product key={prod._id} prod={prod}></Product>
           ))}
         </div>
       </main>
---
+++ mycommerce/frontend/src/components/Product.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Rating from "./Rating";
+
+export default function Product(props) {
+  const { prod } = props;
+  return (
+    <div key={prod._id} className="card">
+      <a href={`/product/${prod._id}`}>
+        <img className="medium" src={prod.image} alt={prod.codename} />
+      </a>
+      <div className="card-body">
+        <a href="product.html">
+          <h2>{prod.codename}</h2>
+        </a>
+        <Rating rating={prod.rating}></Rating>
+        <div className="price">R$ {prod.price.toFixed(2)}</div>
+      </div>
+    </div>
+  );
+}
