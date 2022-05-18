5b924c5e80e42b5f042199c7d436b4ab7ded3718
RenanSiman <renanvinte2@hotmail.com>
2022-05-18 12:01:37 -0300

    create node server

---
+++ mycommerce/backend/data.js
@@ -0,0 +1,80 @@
+const data = {
+  products: [
+    {
+      _id: "1",
+      name: "Thanos",
+      specie: "Titan Eternal",
+      image: "/images/thanos.jpg",
+      price: 400.0,
+      power: [
+        "Super força ilimitada",
+        "Velocidade sobre-humana",
+        "Conhecimento místico",
+      ],
+      rating: 4.5,
+      codename: "Thanos, O Titã Louco",
+      countInStock: 0
+    },
+    {
+      _id: "2",
+      name: "Wanda Maximoff",
+      specie: "Mutante",
+      image: "/images/scarlet-witch.jpg",
+      price: 500.0,
+      power: ["Magia natural amplificada"],
+      rating: 5.0,
+      codename: "Feiticeira Escarlate",
+      countInStock: 54
+    },
+    {
+      _id: "3",
+      name: "Tony Stark",
+      specie: "Humano",
+      image: "/images/iron-man.jpg",
+      price: 340.0,
+      power: ["Gênio", "Playboy", "Bilionário", "Filantropo"],
+      rating: 3.5,
+      codename: "Homem de Ferro",
+      countInStock: 30
+    },
+    {
+      _id: "4",
+      name: "Thor Odinson",
+      specie: "Asgardiano",
+      image: "/images/thor.jpg",
+      price: 420.0,
+      power: [
+        "força sobre-humana",
+        "velocidade sobre-humana",
+        "controla efeitos físicos (trovão, tempestadade, raios)",
+      ],
+      rating: 4,
+      codename: "Thor, Deus do Trovão",
+      countInStock: 25
+    },
+    {
+      _id: "5",
+      name: "Bruce Banner",
+      specie: "Humano Geneticamente Modificado",
+      image: "/images/hulk.png",
+      price: 400.0,
+      power: ["super força ilimitada", "regeneração", "intelecto genial"],
+      rating: 4,
+      codename: "O Incrível Hulk",
+      countInStock: 18
+    },
+    {
+      _id: "6",
+      name: "Peter Parker",
+      specie: "Humano Geneticamente Modificado",
+      image: "/images/spider-man.jpg",
+      price: 250.0,
+      power: ["intelecto genial", "aderencia física", "reflexos sobre-humanos"],
+      rating: 2.5,
+      codename: "Homem Aranha",
+      countInStock: 13
+    },
+  ],
+};
+
+export default data;
